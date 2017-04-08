c65dc69b81d5c71db32699824065ed07e4eb23ec
abhay chaturvedi <abhay chaturvedi>
2017-04-08 18:04:24 +0530

    nxt

--- server.js
+++ server.js
@@ -5,4 +5,14 @@ var path = require('path');
 var fs = require('fs');
 
 var server= "localhost";
-var port = "3000"; 
+var port = 3000; 
+
+var app = express;
+app.use(function (req, res ,next){
+    console.log(req.headers);
+
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    x
+
+})
+
